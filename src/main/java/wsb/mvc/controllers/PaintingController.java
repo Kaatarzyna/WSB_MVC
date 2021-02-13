9a0f1c2c07cad2f158dd33305a7139dde09e894b
Katarzyna Duchowska <k.duchowska@o2.pl>
2021-02-13 22:55:26 +0100

    REST

--- src/main/java/wsb/mvc/controllers/PaintingController.java
+++ src/main/java/wsb/mvc/controllers/PaintingController.java
@@ -2,9 +2,13 @@ package wsb.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import wsb.mvc.models.Painting;
 import wsb.mvc.services.PaintingService;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class PaintingController {
@@ -38,5 +42,20 @@ public class PaintingController {
     @GetMapping("/rest")
     public String rest() {
         return "rest";
+    }
+
+    @GetMapping("/addFile")
+    public String addFileForm() {
+        return "addFile";
+    }
+
+    @PostMapping("/addFile")
+    public String addFile(@RequestParam("file") MultipartFile file, @RequestParam String description) throws IOException {
+
+        System.out.println("Opis: " + description);
+        System.out.println("Nazwa pliku: " + file.getOriginalFilename());
+        System.out.println("Zawartość pliku: " + new String(file.getBytes(), StandardCharsets.UTF_8));
+
+        return "redirect:/";
     }
 }
